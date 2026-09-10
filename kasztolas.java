28e543be2c4a5aa0b4d4a5456f5b0b40245235d0
b2szalev <127097829+b2szalev@users.noreply.github.com>
2026-09-10 09:39:11 +0200

    Add files via upload
    commit

---
+++ kasztolas.java
@@ -0,0 +1,55 @@
+
+    void main(){
+        float lebego=1.23f;
+        long longK=(long)lebego;
+
+        int i=1;
+        char charK=(char)i;
+
+        double d=2.1d;
+        float floatK=(float)d;
+
+        char c='c';
+        short shortK=(short)c;
+
+        String str="alma";
+
+        long l=123;
+        int intK=(int)l;
+
+        short s=456;
+        byte byteK=(byte)s;
+
+        byte b=100;
+        boolean bool=true;
+
+
+
+        System.out.println(lebego);
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(str);
+        System.out.println(l);
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(bool);
+
+        System.out.println("--------------------------");
+
+        //kasztolas print
+        System.out.println(floatK);
+        System.out.println(longK);
+        System.out.println(intK);
+        System.out.println(charK);
+        System.out.println(shortK);
+        System.out.println(byteK);
+
+
+
+
+
+
+
+
+    }
